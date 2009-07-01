551daa1386fa76cb74c7570e5020fc3ad5ea8456
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2009-07-01 15:49:50 +0000

    [IAC-334] Cannot clone component when having multiple feature with undertermined ranges ... and now with the unit tests.

--- intact-core/src/test/java/uk/ac/ebi/intact/model/clone/IntactClonerTest.java
+++ intact-core/src/test/java/uk/ac/ebi/intact/model/clone/IntactClonerTest.java
@@ -11,6 +11,7 @@ import uk.ac.ebi.intact.model.visitor.DefaultTraverser;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * IntactCloner Tester.
@@ -33,6 +34,126 @@ public class IntactClonerTest extends IntactBasicTestCase {
     @Test
     public void cloneInteraction() throws Exception {
         clone( getMockBuilder().createDeterministicInteraction() );
+    }
+
+        @Test
+    public void cloneInteractionWithMultipleFeature() throws Exception {
+        final Interaction interaction = getMockBuilder().createDeterministicInteraction();
+
+        final Iterator<Component> iterator = interaction.getComponents().iterator();
+
+        Component c1 = iterator.next();
+        c1.setShortLabel( "c1" );
+        c1.getBindingDomains().clear();
+        addFeature( c1, CvFeatureType.MUTATION_DECREASING, CvFeatureType.MUTATION_DECREASING_MI_REF, 10, 50, false, "region" );
+        addFeature( c1, CvFeatureType.EXPERIMENTAL_FEATURE, CvFeatureType.EXPERIMENTAL_FEATURE_MI_REF, 20, 25, false, "region" );
+
+        Component c2 = iterator.next();
+        c2.setShortLabel( "c2" );
+        c2.getBindingDomains().clear();
+        addFeature( c2, CvFeatureType.MUTATION_DECREASING, CvFeatureType.MUTATION_DECREASING_MI_REF, 10, 50, false, "region" );
+        addFeature( c2, CvFeatureType.MUTATION_DISRUPTING, CvFeatureType.MUTATION_DISRUPTING_MI_REF, 10, 55, false, "region" );
+
+        final Interaction clone = cloner.clone( interaction );
+//        final Interaction clone = new InteractionIntactCloner().clone( interaction );
+
+        for ( Component component : clone.getComponents() ) {
+            if( component.getShortLabel().equals( "c1" ) ) {
+
+                Assert.assertEquals(2, component.getBindingDomains().size());
+                Assert.assertTrue( "Component c1 is lacking at least one feature",
+                                   hasFeature( component, "region", CvFeatureType.MUTATION_DECREASING, 10, 50 )
+                                   || hasFeature( component, "region", CvFeatureType.EXPERIMENTAL_FEATURE, 20, 25 ) );
+
+            } else if( component.getShortLabel().equals( "c2" ) ) {
+
+                Assert.assertEquals(2, component.getBindingDomains().size());
+                Assert.assertTrue( "Component c2 is lacking at least one feature",
+                                   hasFeature( component, "region", CvFeatureType.MUTATION_DECREASING, 10, 50 )
+                                   || hasFeature( component, "region", CvFeatureType.MUTATION_DISRUPTING, 10, 55 ) );
+            } else {
+                Assert.fail();
+            }
+        }
+    }
+
+    @Test
+    public void cloneInteractionWithMultipleFeature_underterminedRanges() throws Exception {
+        final Interaction interaction = getMockBuilder().createDeterministicInteraction();
+
+        final Iterator<Component> iterator = interaction.getComponents().iterator();
+
+        final boolean undetermined = true;
+
+        Component c1 = iterator.next();
+        c1.setShortLabel( "c1" );
+        c1.getBindingDomains().clear();
+        addFeature( c1, CvFeatureType.MUTATION_DECREASING, CvFeatureType.MUTATION_DECREASING_MI_REF, 0, 0, undetermined, "region" );
+        addFeature( c1, CvFeatureType.EXPERIMENTAL_FEATURE, CvFeatureType.EXPERIMENTAL_FEATURE_MI_REF, 0, 0, undetermined, "region" );
+
+        Component c2 = iterator.next();
+        c2.setShortLabel( "c2" );
+        c2.getBindingDomains().clear();
+        addFeature( c2, CvFeatureType.MUTATION_DECREASING, CvFeatureType.MUTATION_DECREASING_MI_REF, 0, 0, undetermined, "region" );
+        addFeature( c2, CvFeatureType.MUTATION_DISRUPTING, CvFeatureType.MUTATION_DISRUPTING_MI_REF, 0, 0, undetermined, "region" );
+
+        final Interaction clone = cloner.clone( interaction );
+
+        for ( Component component : clone.getComponents() ) {
+            if( component.getShortLabel().equals( "c1" ) ) {
+
+                Assert.assertEquals(2, component.getBindingDomains().size());
+                Assert.assertTrue( "Component c1 is lacking at least one feature",
+                                   hasFeature( component, "region", CvFeatureType.MUTATION_DECREASING, 0, 0 )
+                                   || hasFeature( component, "region", CvFeatureType.EXPERIMENTAL_FEATURE, 0, 0 ) );
+
+            } else if( component.getShortLabel().equals( "c2" ) ) {
+
+                Assert.assertEquals(2, component.getBindingDomains().size());
+                Assert.assertTrue( "Component c2 is lacking at least one feature",
+                                   hasFeature( component, "region", CvFeatureType.MUTATION_DECREASING, 0, 0 )
+                                   || hasFeature( component, "region", CvFeatureType.MUTATION_DISRUPTING, 0, 0 ) );
+            } else {
+                Assert.fail();
+            }
+        }
+    }
+
+    private boolean hasFeature( Component component, String label, String type, int start, int stop ) {
+        boolean foundByLabel = false;
+        for ( Feature feature : component.getBindingDomains() ) {
+            if( label.equals( feature.getShortLabel() ) ) {
+                foundByLabel = true;
+                if( type.equals( feature.getCvFeatureType().getShortLabel() ) ) {
+                    final Range range = feature.getRanges().iterator().next();
+                    if( range.getFromIntervalStart() == start && range.getToIntervalStart() == stop ) {
+                        return true;
+                    } else {
+                        System.out.println( "Failed on range" );
+                    }
+                } else {
+                    System.out.println( "Failed on type" );
+                }
+            }
+        }
+
+        if( !foundByLabel ) {
+            System.out.println( "Failed on Label" );
+        }
+
+        return false;
+    }
+
+    private void addFeature( Component component, String type, String typeMi, int start, int stop, boolean undertermined, String shortlabel ) {
+        CvFeatureType featureType = getMockBuilder().createCvObject( CvFeatureType.class, typeMi, type );
+        Feature feature = getMockBuilder().createFeature( shortlabel, featureType );
+        feature.setComponent(null);
+
+        Range range = getMockBuilder().createRange( start, start, stop, stop );
+        range.setUndetermined( undertermined );
+        range.setLinked( false );
+        feature.addRange(range);
+        component.getBindingDomains().add( feature );
     }
 
     @Test
@@ -110,7 +231,6 @@ public class IntactClonerTest extends IntactBasicTestCase {
         Assert.assertNotNull( clonedComponent.getExperimentalRoles() );
         Assert.assertEquals( 2, clonedComponent.getExperimentalRoles().size() );
     }
-
 
     @Test
     public void clone_cloneCvObjectTree() throws Exception {
