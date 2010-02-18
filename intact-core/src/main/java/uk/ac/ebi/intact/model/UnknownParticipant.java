07025ce462441e5a1e966936f20faafb1d88246b
arnaud.ceol <arnaud.ceol@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-02-18 13:19:30 +0000

    dd CvInteractorType for polysaccharide, biopolymer and unknown participant. All are used by MatrixDb and may now be imported in Intact

---
+++ intact-core/src/main/java/uk/ac/ebi/intact/model/UnknownParticipant.java
@@ -0,0 +1,17 @@
+/*
+Copyright (c) 2002 The European Bioinformatics Institute, and others.  
+All rights reserved. Please see the file LICENSE 
+in the root directory of this distribution.
+*/
+package uk.ac.ebi.intact.model;
+
+/**
+ * @author aceol
+ */
+public interface UnknownParticipant extends Interactor {
+
+} // end UnknownParticipant
+
+
+
+
