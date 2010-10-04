2fdc8843423796177b24862b40b1d8d3f9c17d99
marine.dumousseau@wanadoo.fr <marine.dumousseau@wanadoo.fr@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-10-04 15:28:31 +0000

    Added a method in ProteinUtils to check if a protein is a feature chain

--- intact-core/src/main/java/uk/ac/ebi/intact/model/util/ProteinUtils.java
+++ intact-core/src/main/java/uk/ac/ebi/intact/model/util/ProteinUtils.java
@@ -222,4 +222,22 @@ public class ProteinUtils {
         }
         return false;
     }
+
+    /**
+     * Checks if the current protein is a chain
+     * @param protein the protein to check
+     * @return true if the protein is a chain
+     */
+    public static boolean isFeatureChain(Protein protein) {
+        Collection<InteractorXref> xrefs = protein.getXrefs();
+        for (InteractorXref xref : xrefs) {
+            if (xref.getCvXrefQualifier() != null) {
+                String qualifierIdentity = xref.getCvXrefQualifier().getIdentifier();
+                if (CvXrefQualifier.CHAIN_PARENT.equals(qualifierIdentity)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
