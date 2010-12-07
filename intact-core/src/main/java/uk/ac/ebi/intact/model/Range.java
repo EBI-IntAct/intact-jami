ca3fc721022695e094fc3d8c72fc380bf63131c0
brunoaranda <brunoaranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-12-07 13:50:46 +0000

    Using @Lob of type text

--- intact-core/src/main/java/uk/ac/ebi/intact/model/Range.java
+++ intact-core/src/main/java/uk/ac/ebi/intact/model/Range.java
@@ -995,6 +995,7 @@ public boolean isUndetermined() {
     }
 
     @Lob
+    @Type(type = "text")
     @Column(name = "full_sequence")
     public String getFullSequence() {
         return fullSequence;
