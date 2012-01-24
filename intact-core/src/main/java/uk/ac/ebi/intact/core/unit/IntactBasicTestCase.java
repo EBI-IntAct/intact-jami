4fc2213afc17082f56402725fcb8232b34c82d89
brunoaranda <brunoaranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2012-01-24 14:16:33 +0000

    Removed specific surefire config

--- intact-core/src/main/java/uk/ac/ebi/intact/core/unit/IntactBasicTestCase.java
+++ intact-core/src/main/java/uk/ac/ebi/intact/core/unit/IntactBasicTestCase.java
@@ -21,6 +21,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
